e7c33c22f977d60b396546de4db0fce0b09eb6fe
klaudio0707 <claudioroberto0707@outlook.com>
2026-01-15 14:47:50 -0300

    feat:(app.py) cria função para filtrar arquivo

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PyPDF2 import PdfReader # Biblioteca para ler PDF
 from ai_service import analyze_email
+
+app = Flask(__name__)
+CORS(app)
+
+def extract_text_from_pdf(file):
+    try:
+        reader = PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+        return text
+    except Exception as e:
+        print(f"Erro ao ler PDF: {e}")
+        return ""
